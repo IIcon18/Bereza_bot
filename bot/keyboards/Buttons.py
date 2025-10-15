54599fc4fe88776b49d0a41d1e46094446922d04
svyatoslavgrigorev <Svyatoslav.gr@inbox.ru>
2025-10-15 23:43:08 +0300

    Rename inline.py, change keyboard style

---
+++ bot/keyboards/Buttons.py
@@ -0,0 +1,25 @@
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup, KeyboardButton
+from bot.config.config import VIDEO_TUTORIAL_LINK, SUPPORT_USERNAME
+
+
+def main_menu():
+    "Главная менюшка"
+    keyboard = ReplyKeyboardMarkup(
+        keyboard=[
+            [KeyboardButton(text= "🧾 Моя подписка")],
+            [KeyboardButton(text= "💬 Поддержка")],
+        ],
+        resize_keyboard=True,
+        one_time_keyboard=False
+    )
+    return keyboard
+
+
+def back_button():
+    "Кнопка назад"
+    keyboard = ReplyKeyboardMarkup(
+        keyboard=[
+            [KeyboardButton(text= "↩︎ Назад")],
+        ]
+    )
+    return keyboard
